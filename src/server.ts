6a9135b837b8d249fb5b088abc933bac517d17a5
nocodenodev <167807133+nocodenodev@users.noreply.github.com>
2025-06-07 20:10:55 -0300

    feat(users): implement user creation route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createUser } from "./routes/users/createUser";
 
 const app = fastify();
 
 app.get("/", () => {
   return "Hello World";
 });
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createUser);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
