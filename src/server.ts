7c2fee5b286f4b0f61c6958562075caa5b10801f
nocodenodev <167807133+nocodenodev@users.noreply.github.com>
2025-06-07 21:39:19 -0300

    feat(auth): implement JWT authentication flow

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import fastifyCors from "@fastify/cors";
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createUser } from "./routes/users/createUser";
+import fastifyJwt from "@fastify/jwt";
+import { pegarUserPeloId } from "./routes/users/get-user-by-id";
+import { login } from "./routes/auth/login";
 
 const app = fastify();
 
@@ -14,12 +17,36 @@ app.get("/", () => {
 
 app.register(fastifyCors, {
   origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 });
+
+app.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET_KEY || "supersecretkey",
+  sign: {
+    expiresIn: "1d",
+  },
+});
+
+app.decorate(
+  "authenticate",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (err) {
+      reply.code(401).send({
+        error: "Não autorizado. Token inválido ou expirado.",
+      });
+    }
+  }
+);
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createUser);
+app.register(pegarUserPeloId);
+app.register(login);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
